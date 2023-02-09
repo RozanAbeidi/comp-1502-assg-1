b7fa94cccbe3aa672367581740a8efa4c31abfb5
RozanAbeidi <rabei871@mtroyal.ca>
2023-02-08 19:45:41 -0700

    message

---
+++ src/mru/game/controller/CardHand.java
@@ -0,0 +1,35 @@
+package mru.game.controller;
+
+import java.util.ArrayList;
+public class CardHand {
+
+
+private ArrayList<Card> heldCards;
+
+public CardHand(ArrayList<Card> cards) {
+	 heldCards = cards;
+}
+public CardHand() {
+	heldCards = new ArrayList<>();
+}
+public  int  score() {
+	int totalScore = 0;
+	
+	for (Card card: heldCards) {
+		totalScore = totalScore +card.getRank();
+	}
+	return totalScore % 10;
+	
+
+
+}
+public void add(Card card) {
+	heldCards.add(card);
+}
+//
+//public static void main(String[] args) {
+//CardDeck deck = new CardDeck();
+//CardHand playerhand = new CardHand(new ArrayList<Card>());
+//CardHand bankerhand = new CardHand(new ArrayList<Card>());
+
+}
---
+++ src/mru/game/controller/PuntoBancoGame.java
@@ -0,0 +1,45 @@
+package mru.game.controller;
+
+
+public class PuntoBancoGame {
+	
+	/**
+	 * In this class you implement the game
+	 * You should use CardDeck class here
+	 * See the instructions for the game rules
+	 */
+private CardDeck deck; 
+
+public String winner() {
+	if (playerhand.score() > bankerhand.score ()){
+		 return "playerHand wins"
+		}
+
+//		if (playerhand.getSum() < bankerhand.getSum()){
+//		system.out.println("bankerhand wins");	
+//		}
+//		if (playerhand.getSum()== bankerhand.getSum()) {
+//		system.out.println("tie");
+//		}
+//			
+//		if (playerhand.getSum = 8 || 9);{
+//		return score;
+//
+//		if (bankerhand.getSum == 8 || 9{
+//		}
+//		return score;
+//
+//		if playerhand.getSum =0=>5{
+//			card++
+//		}
+//
+//		if get.third 
+//		}
+//
+//
+//Sum = score 
+
+
+		}
+
+}
