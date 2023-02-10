0c9c86d9563c79999573dfa0b5500fbdbac5c920
Md-Junayed-Talukdar <mtalu597@mtroyal.ca>
2023-02-09 23:24:45 -0700

    testing

--- src/mru/game/controller/GameManager.java
+++ src/mru/game/controller/GameManager.java
@@ -1,5 +1,15 @@
 package mru.game.controller;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import mru.game.model.Player;
+import mru.game.view.AppMenu;
 
 public class GameManager {
 	
@@ -11,8 +21,111 @@ public class GameManager {
 	 * A method to find the top players
 	 * Depending on your designing technique you may need and you can add more methods here 
 	 */
+	//Array list for storing the player information
+	
+	ArrayList<Player> players;
+	
+	// Constant File Path
+	
+	private final String FILE_PATH = "res/CasinoInfo.txt";
+	File db = new File(FILE_PATH);
+	
+	// For showing menu we created a object from App menu class named as show menu
+	AppMenu showMenu;
+	
+	
+	public GameManager() throws FileNotFoundException {
+		players = new ArrayList <>();
+		loadData();
+		launchApplication();
+	}
+	private void launchApplication() {
+		boolean stopLoop = true;
+		char option;
+		while (stopLoop) {
+			option = showMenu.showMainMenu();
+			switch (option) {
+			case 't':
+				playgame();
+				break;
+			case 'e':
+				search();
+				break;
+			case 's':
+				save();
+				break;
+				stopLoop = false;
+			}
+		}
+		
+	}
+	private void save() throws IOException {
+		PrintWriter pw = new PrintWriter(db);
+		
+		System.out.println("saving files please wait");
+		for (Player items: players) {
+			pw.println(items.format());
+		}
+		pw.close();
+		
+	}
+	private void search() {
+		showMenu.showSubMenu();
+		char option;
+		boolean flag = true;
+		while (flag) {
+			switch (option) {
+			case 't':
+				topPlayer();
+				break;
+			case 'n':
+				Player nameFound = lookForName(); 
+				break;
+			case 'b':
+				showMenu.showMainMenu();
+				break;
+				flag = false;
+			}
+		}
+		
+	}
+	private Player lookForName() {
+		String name = showMenu.promptName();
+		Player nameFound = null;
+		for (Player names: players) {
+			if (names.getName().equals(name)) {
+				nameFound = names;
+				break;
+			}
+			else {
+				System.out.println("Player not found returning to main menu");
+			}
+		}
+		return nameFound;
+	}
+	// checks the file it exists or not at startup
+	private void loadData() throws FileNotFoundException {
+		String [] splittedLines;
+		String lines;
+		String name;
+		int balance;
+		int numberOfWins;
+		
+		if(db.exists()) {
+			Scanner fileReader = new Scanner(db);
+			while (fileReader.hasNextLine()) {
+				lines = fileReader.nextLine();
+				splittedLines = lines.split(",");
+				name = splittedLines[0];
+				balance = Integer.parseInt(splittedLines[1]);
+				numberOfWins = Integer.parseInt(splittedLines[2]);
+				Player playerInfo = new Player(name, balance, numberOfWins);
+				players.add(playerInfo);
+				fileReader.close();
+			}
+		}
+	}
 
 	
 
 }
-
--- src/mru/game/view/AppMenu.java
+++ src/mru/game/view/AppMenu.java
@@ -1,10 +1,51 @@
 package mru.game.view;
 
+import java.util.Scanner;
+
+import mru.game.model.Player;
 
 public class AppMenu {
-
+	
 	/**
 	 * This class will be used to show the menus and sub menus to the user
 	 * It also prompts the user for the inputs and validates them 
 	 */
+	Scanner keyboard;
+	// Constructor for the AppMenu class
+	public AppMenu() {
+		keyboard = new Scanner(System.in);
+	}
+	// printing the main menu
+	public char showMainMenu() {
+		char option;
+		System.out.println("Select one of this options");
+		System.out.println("\t(P) Play game");
+		System.out.println("\t(S) Search");
+		System.out.println("\t(E) Exit");
+		System.out.println("Enter your choice");
+		option = keyboard.nextLine().toLowerCase().charAt(0);
+		return option;
+		
+	}
+	
+	//printing the sub menu
+	public char showSubMenu() {
+		char option;
+		System.out.println("Select one of this options:");
+		System.out.println("\t(T) Top player (Most Number Of Wins");
+		System.out.println("\t(N) Looking For a Name?");
+		System.out.println("\t(B) Back To Main Menu");
+		System.out.println("Enter your choice");
+		option = keyboard.nextLine().toLowerCase().charAt(0);
+		return option;
+	}
+	
+	public String promptName() {
+		 System.out.print("What is your name sir? Name: ");
+		 String name = keyboard.nextLine().trim();
+		 return name;
+	}
+	public void foundPlayer(Player nameFound) {
+		System.out.println(nameFound);
+	}
 }
