e38d24ca87653efd1bf5d62b41fff468e263a51a
RozanAbeidi <rabei871@mtroyal.ca>
2023-02-14 14:13:16 -0700

    message

--- src/mru/game/controller/CardDeck.java
+++ src/mru/game/controller/CardDeck.java
@@ -5,18 +5,20 @@ import java.util.Collections;
 
 /**
  * This class represents a card deck
+ * 
  * @author ksalmani
  * @version 1.0
  */
 public class CardDeck {
-	
+
 	/**
 	 * deck holds all of the cards that currently are in the current deck
 	 */
-	private ArrayList <Card> deck;
-	
+	private ArrayList<Card> deck;
+
 	/**
-	 * This constructor initiate the arraylist and calls the repective methods to create a new deck
+	 * This constructor initiate the arraylist and calls the repective methods to
+	 * create a new deck
 	 */
 	public CardDeck() {
 		deck = new ArrayList<Card>();
@@ -29,37 +31,37 @@ public class CardDeck {
 	 */
 	private void createDeck() {
 		// suits holds the name of the suits
-		String[] suits = {"Spades", "Diamond", "Clubs", "Hearts"};
-		
+		String[] suits = { "Spades", "Diamond", "Clubs", "Hearts" };
+
 		/*
 		 * The for loop creates a whole new deck based on their suit and rank
 		 */
-		for (int i = 0 ; i < 4; i++) {
-			for (int j = 1 ; j <=13 ; j++) {
-				deck.add(new Card (j,suits[i]));
+		for (int i = 0; i < 4; i++) {
+			for (int j = 1; j <= 13; j++) {
+				deck.add(new Card(j, suits[i]));
 			}
 		}
-		
+
 	}
 
 	/**
 	 * this method shuffle the deck after creating a new deck
 	 */
 	private void shuffleDeck() {
-		Collections.shuffle(deck); 
+		Collections.shuffle(deck);
 	}
 
-	public Card getCard() {
+	public Card drawCard() {
 		return deck.remove(0);
-		 
-		 
-		
+
 	}
+
 	/**
 	 * The deck getter method
+	 * 
 	 * @return the deck
 	 */
-	public ArrayList<Card> getDeck() {
+	public ArrayList<Card> drawDeck() {
 		return deck;
 	}
 }
--- src/mru/game/controller/PuntoBancoGame.java
+++ src/mru/game/controller/PuntoBancoGame.java
@@ -1,50 +1,122 @@
 package mru.game.controller;
 
-
 public class PuntoBancoGame {
-	
+	public PuntoBancoGame() {
+		deck = new CardDeck();
+	}
+
+	public static void main(String[] args) {
+		PuntoBancoGame puntoBancoGame = new PuntoBancoGame();
+		String winner = puntoBancoGame.winner();
+		System.out.print(winner);
+
+		// make a PuntoBancoGame object
+		// call its winner() method and print out the result
+
+	}
+
 	/**
-	 * In this class you implement the game
-	 * You should use CardDeck class here
-	 * See the instructions for the game rules
+	 * In this class you implement the game You should use CardDeck class here See
+	 * the instructions for the game rules
 	 */
-private CardDeck deck; 
+	private CardDeck deck;
 
-public String winner() {
-	CardHand playerhand = new CardHand();
-	CardHand bankerhand = new CardHand();
-	
-	playerhand.add
-	
-	if (playerhand.score() > bankerhand.score()){
-		 return "playerHand wins"
+	public String winner() {
+		CardHand playerhand = new CardHand();
+		CardHand bankerhand = new CardHand();
+		Card card = deck.drawCard();
+		Card thirdCard = null;
+
+		playerhand.add(card);
+		card = deck.drawCard();
+		bankerhand.add(card);
+		card = deck.drawCard();
+		playerhand.add(card);
+		card = deck.drawCard();
+		bankerhand.add(card);
+
+		if (playerhand.score() == 8 || playerhand.score() == 9 || bankerhand.score() == 8 || bankerhand.score() == 9) {
+
+			return result(playerhand, bankerhand);
+		}
+		if (playerhand.score() <= 5) {
+			thirdCard = deck.drawCard();
+			playerhand.add(thirdCard);
 		}
 
-//		if (playerhand.getSum() < bankerhand.getSum()){
-//		system.out.println("bankerhand wins");	
-//		}
-//		if (playerhand.getSum()== bankerhand.getSum()) {
-//		system.out.println("tie");
-//		}
+		if (thirdCard == null) {
+			if (bankerhand.score() <= 5) {
+				card = deck.drawCard();
+				bankerhand.add(card);
+			}
+
+		} else {
+
+			if (playerhand.score() == 2 || playerhand.score() == 3) {
+				if (bankerhand.score() <= 4) {
+					card = deck.drawCard();
+					bankerhand.add(card);
+				}
+			} else if (playerhand.score() == 4 || playerhand.score() == 5) {
+				if (bankerhand.score() <= 5) {
+					card = deck.drawCard();
+					bankerhand.add(card);
+				}
+			}
+
+			else if (playerhand.score() == 6 || playerhand.score() == 7) {
+				if (bankerhand.score() <= 6) {
+					card = deck.drawCard();
+					bankerhand.add(card);
+
+				}
+			} else if (playerhand.score() == 8) {
+				if (bankerhand.score() <= 2) {
+					card = deck.drawCard();
+					bankerhand.add(card);
+
+				}
+			} else if (playerhand.score() == 1 || playerhand.score() == 9 || playerhand.score() == 10
+					|| playerhand.score() == 0) {
+				if (bankerhand.score() <= 3)
+					;
+				card = deck.drawCard();
+				bankerhand.add(card);
+
+			}
+
+		}
+
+		System.out.println(playerhand);
+		System.out.println(bankerhand);
+
+		return result(playerhand, bankerhand);
+	}
+
+	public String result(CardHand playerhand, CardHand bankerhand) {
+
+		if (playerhand.score() > bankerhand.score()) {
+
+			return "playerhand wins";
+		}
+
+		if (playerhand.score() < bankerhand.score()) {
+
+			return "bankerhand wins";
+		}
+
+		return "tie";
+	}
+
+//	CardDeck fromDeck = new CardDeck();
+//	Card currentCard = fromDeck.getDeck().remove(0);
+//	
+
+//	
 //			
-//		if (playerhand.getSum = 8 || 9);{
-//		return score;
-//
-//		if (bankerhand.getSum == 8 || 9{
-//		}
-//		return score;
-//
-//		if playerhand.getSum =0=>5{
-//			card++
-//		}
-//
-//		if get.third 
-//		}
-//
-//
-//Sum = score 
+//	
 
-
-		}
-
+//			
+//	CardDeck fromDeck = new CardDeck();
+//	Card currentCard = fromDeck.getDeck().remove(0);
 }
